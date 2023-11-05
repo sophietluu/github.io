7566bd3b98194004ecdd96100049e3fc316aaa69
solu7850 <sophie.luu@colorado.edu>
2023-11-04 21:30:13 -0600

    updates

--- js/wa10.js
+++ js/wa10.js
@@ -1,5 +1,3 @@
-// Note: Story is generated using AI
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -9,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = "It was a sunny day, with a temperature of :temperature: degrees, so :insertx: decided to go on an adventure. They packed their bags and headed to :inserty:. Along the way, they encountered many exciting things and met new friends. At one point, they even had a picnic with :insertz:. It was a memorable journey, and they returned home with wonderful memories. :insertx: couldn't wait to share their stories with their other stuffed animal friends.";
+const storyText = "It was a sunny day, with a temperature of :temperature:, so :insertx: decided to go on an adventure. They packed their bags and headed to :inserty:. Along the way, they encountered many exciting things and met new friends. At one point, they even had a picnic with :insertz:. It was a memorable journey, and they returned home with wonderful memories. :insertx: couldn't wait to share their stories with their other stuffed animal friends.";
 const insertX = ['Teddy the Bear', 'Lily the Bunny', 'Max the Dog'];
 const insertY = ['the beach', 'the mountains', 'a bustling city'];
 const insertZ = ['a friendly squirrel', 'a wise owl', 'a playful kitten'];
@@ -33,9 +31,9 @@ function result() {
 
     if(document.getElementById("uk").checked) {
       const temperature = Math.round((94 - 32) * 5/9);
-      newStory = newStory.replace(/:temperature:/g, temperature);
+      newStory = newStory.replace(/:temperature:/g, temperature + " degrees celsius");
     } else {
-      newStory = newStory.replace(/:temperature:/g, 94);
+      newStory = newStory.replace(/:temperature:/g, "94 degrees fahrenheit");
     }
 
     story.textContent = newStory;
